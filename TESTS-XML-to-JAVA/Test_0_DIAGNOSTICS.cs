f7154ceed38499da7a6510e007c011ae588e7634
Json2CSharp <hilalhakla@gmail.com>
2022-10-31 19:49:36 +0300

    Update Tests

--- TESTS-XML-to-JAVA/Test_0_DIAGNOSTICS.cs
+++ TESTS-XML-to-JAVA/Test_0_DIAGNOSTICS.cs
@@ -16,13 +16,14 @@ namespace TESTS_XML_to_JAVA
         [TestMethod]
         public void Run()
         { 
+            Assert.Inconclusive(message: "This test is just for debugging.");
+            return;
             string path = Directory.GetCurrentDirectory().Replace("bin\\Debug\\netcoreapp3.1", "")  + @"Test_0_DIAGNOSTICS_INPUT.txt";
             string resultPath =  Directory.GetCurrentDirectory().Replace("bin\\Debug\\netcoreapp3.1", "") + @"Test_0_DIAGNOSTICS_OUTPUT.txt";            
             string input = File.ReadAllText(path);
             string errorMessage = string.Empty;
             string returnVal = new Xml2CodeConverter(new JavaClassWriter(null)).Convert(input, out errorMessage);
             string resultsCompare = File.ReadAllText(resultPath); 
-            Assert.Inconclusive(message: "This test is just for debugging.");
             Assert.AreEqual(resultsCompare.Replace(Environment.NewLine, "").Replace(" ", "").Replace("\t", ""), returnVal.Replace(Environment.NewLine, "").Replace(" ", "").Replace("\t", ""));
         }
     }
