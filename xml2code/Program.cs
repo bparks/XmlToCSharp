a56224272d44ac027b109b49e07f1f99d20125b0
Brian Parks <bparks@brianparks.me>
2024-03-14 21:35:44 -0700

    Update to .net7/8 and add a CLI for converting to csharp

---
+++ xml2code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xml2CSharp;
+using Xml2CSharp.ClassWriters;
+
+//Console.WriteLine(args.First());
+
+var codeWriter = new CSharpClassWriter(new System.Collections.Generic.Dictionary<string, object>
+{
+    ["UsePascalCase"] = true
+});
+var result = new Xml2CodeConverter(codeWriter).Convert(File.ReadAllText(args.First()), out var error);
+if (error is not null)
+{
+    Console.Error.WriteLine(error);
+    Environment.Exit(1);
+}
+Console.WriteLine(result);
